ed872f90bd0e2c25c7e018e5752c5211624f9cd3
dldbdlf <69393474+dldbdlf@users.noreply.github.com>
2025-01-23 15:38:46 +0900

    Title: 블럭쌓는 명령2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 250123/블럭쌓는 명령2/block-stacking-commands2.c
+++ 250123/블럭쌓는 명령2/block-stacking-commands2.c
@@ -4,14 +4,14 @@ int main() {
     int size;
     int sequence;
     scanf("%d %d", &size, &sequence);
-    int blocks[7] ={0,0,0,0,0,0,0};
+    int blocks[8] ={0,0,0,0,0,0,0,0};
     for(int i =0; i< sequence; ++i)
     {
         int start, end; 
         scanf("%d %d",&start, &end);
         for(int j = start; j < end+1; ++j)
         {
-            blocks[j-1] += 1;
+            blocks[j] += 1;
         }
     }
     int max =0;

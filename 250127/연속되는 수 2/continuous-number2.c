79c720b3df44378958884ffa054add9218043817
dldbdlf <69393474+dldbdlf@users.noreply.github.com>
2025-01-27 10:57:30 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250127/연속되는 수 2/continuous-number2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    scanf("%d",&number);
+    int cnt =1;
+    int max_cnt = 0;
+    int before = -1;
+
+    for(int i =0; i < number; ++i)
+    {
+        int num;
+        scanf("%d", &num);
+        if(before == num)
+        {
+            cnt += 1;
+        }
+        else
+        {
+            if(max_cnt < cnt)
+            {
+                max_cnt = cnt;
+            }
+            cnt = 1;
+            before = num;
+        }
+    }
+    printf("%d",max_cnt);
+    return 0;
+}

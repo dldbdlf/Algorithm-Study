3a469099cd31a08b132dd1dfcec14e6b33c93322
dldbdlf <69393474+dldbdlf@users.noreply.github.com>
2025-02-02 15:23:24 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250202/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    scanf("%d", &number);
+    int cows[number];
+    for(int i =0; i < number; ++i)
+    {
+        scanf("%d",&cows[i]);
+    }
+    int result = 0;
+    for(int i =0; i < number -2 ; ++i)
+    {
+        int first = cows[i];
+        for(int j = i+1; j < number -1; ++j)
+        {
+            if(cows[j] > first)
+            {
+                int second = cows[j];
+                for(int z = j+1; z< number; ++z)
+                {
+                    if(cows[z] > second)
+                    {
+                        result += 1;
+                    }
+                }
+            }
+        }
+    }
+    printf("%d", result);
+    return 0;
+}

4d7350a844143098eac71f8b164df9bffd31366c
dldbdlf <69393474+dldbdlf@users.noreply.github.com>
2025-07-04 17:46:04 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250704/반복 출력하기/repeated-output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int row_num;
+    scanf("%d", &row_num);
+    for(int i =0; i < row_num; i++)
+    {
+        printf("12345^&*()_\n");
+    }
+    return 0;
+}
